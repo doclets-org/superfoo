58e954a5a0d77215e9a2d34b73d1ed62de4e99e1
Gerhard Preuss <gelipp@gmail.com>
2016-03-18 09:13:42 +0100

    asd

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Does something really weired.
+ *
+ * @function
+ * @param {string} mode
+ * @return {number} The world's most precious number
+ *
+ */
+var makeItHappen = function(mode) {
+	console.log(mode)
+	return 1.1112
+}
